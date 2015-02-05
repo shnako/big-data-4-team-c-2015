aa85a1e330831f8200c562500828a9cad24d3763
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2015-02-05 09:22:14 +0000

    Factored out common code to helpers and a little refactoring.

--- src/Task1/Task1Reducer.java
+++ src/Task1/Task1Reducer.java
@@ -1,39 +1,22 @@
 package Task1;
 
+import helpers.Helpers;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 public class Task1Reducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
     public void reduce(IntWritable articleId, Iterable<IntWritable> revisionIds, Context context) throws InterruptedException, IOException {
         int revisionCount = 0;
         StringBuilder revisions = new StringBuilder();
 
-        for (Integer revisionId : getSortedCollection(revisionIds) ) {
+        for (Integer revisionId : Helpers.getSortedIntWritableCollection(revisionIds)) {
             revisions.append(revisionId).append(" ");
             revisionCount++;
         }
 
-        StringBuilder finalVal = new StringBuilder().append(revisionCount).append(" ").append(revisions);
-        context.write(articleId, new Text(finalVal.toString().trim()));
-    }
-
-    private Integer[] getSortedCollection(Iterable<IntWritable> revisionIds) {
-        List<Integer> list = new ArrayList<Integer>();
-        Integer[] dummy = new Integer[0];
-        for (IntWritable revision : revisionIds) {
-            list.add(revision.get());
-        }
-
-        Collections.sort(list);
-
-        return list.toArray(dummy);
+        context.write(articleId, new Text((String.valueOf(revisionCount) + " " + revisions).trim()));
     }
 }
---
+++ src/helpers/Helpers.java
@@ -0,0 +1,47 @@
+package helpers;
+
+import org.apache.hadoop.io.IntWritable;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class Helpers {
+    public static final String ISO8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+    public static Date convertTimestampToDate(String timestamp) {
+        try {
+            DateFormat iso8601Format = new SimpleDateFormat(ISO8601_FORMAT);
+            return iso8601Format.parse(timestamp);
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    public static boolean isInTimeFrame(String startDate, String endDate, String timestamp) {
+        try {
+            DateFormat iso8601Format = new SimpleDateFormat(ISO8601_FORMAT);
+            Date timestampDate = convertTimestampToDate(timestamp);
+            return iso8601Format.parse(startDate).before(timestampDate) && timestampDate.before(iso8601Format.parse(endDate));
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static Integer[] getSortedIntWritableCollection(Iterable<IntWritable> revisionIds) {
+        List<Integer> list = new ArrayList<Integer>();
+        Integer[] dummy = new Integer[0];
+        for (IntWritable revision : revisionIds) {
+            list.add(revision.get());
+        }
+
+        Collections.sort(list);
+
+        return list.toArray(dummy);
+    }
+}
