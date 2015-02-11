de3dc05faf7522c3a7ad1d2defd5ffda78c13cad
Mircea Iordache <1106729i@student.gla.ac.uk>
2015-02-11 18:36:19 +0000

    Random date-parsing stuff

--- src/mappers/FrequencyMapper.java
+++ src/mappers/FrequencyMapper.java
@@ -22,12 +22,30 @@ public class FrequencyMapper extends Mapper<Object, Text, IntWritable, IntWritab
     }
 
     public void map(Object key, Text value, Context context) throws InterruptedException, IOException {
-        String[] tokens = Helpers.fastStartsWithAndTokenize(4, value.toString(), Helpers.REVISION_TAG);
-        if (tokens != null) {
+    /*    StringTokenizer tokenizer = new StringTokenizer(value.toString());
+        // Ensure we only process the lines containing the REVISION tag which have the correct number of tokens.
+        if (tokenizer.hasMoreTokens() && tokenizer.nextToken().equals(Helpers.REVISION_TAG)) {
+            String articleId = tokenizer.nextToken();
+            String revisionId = tokenizer.nextToken();
+            tokenizer.nextToken(); // Skip the article title.
+            // If the timestamp is between the specified dates, output it.
+            Date startDate = Helpers.convertTimestampToDate(startDateString);
+            Date endDate = Helpers.convertTimestampToDate(endDateString);
+            Date timestamp = Helpers.convertTimestampToDate(tokenizer.nextToken());
+            if ((startDate.before(timestamp) || startDate.equals(timestamp)) && (timestamp.before(endDate) || endDate.equals(timestamp))) {
+                context.write(new IntWritable(Integer.parseInt(articleId)), new IntWritable(Integer.parseInt(revisionId)));
+            }
+        }
+    } */
+
+    String[] tokens = Helpers.fastStartsWithAndTokenize(4, value.toString(), Helpers.REVISION_TAG);
+    if (tokens != null) {
             // If the timestamp is between the specified dates, output it.
             Date startDate = Helpers.convertTimestampToDate(startDateString);
             Date endDate = Helpers.convertTimestampToDate(endDateString);
             Date timestamp = Helpers.convertTimestampToDate(tokens[3]);
+            if (timestamp == null)
+                System.out.println("Value is: "+value);
             if ((startDate.before(timestamp) || startDate.equals(timestamp)) && (timestamp.before(endDate) || endDate.equals(timestamp))) {
                 context.write(new IntWritable(Integer.parseInt(tokens[0])), new IntWritable(Integer.parseInt(tokens[1])));
             }
