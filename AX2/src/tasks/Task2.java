011a45e46d0d2f573530bf9e4ae08e29ccc02390
Mircea Iordache <1106729i@student.gla.ac.uk>
2015-03-10 20:40:55 +0000

    Made AX2 compile. Need to do optimisation

--- AX2/src/tasks/Task2.java
+++ AX2/src/tasks/Task2.java
@@ -1,71 +1,87 @@
 package tasks;
 
+import helpers.ArticleRevCountWritable;
+import helpers.FilePrinter;
+import mappers.FrequencyMapper;
+import mappers.TopKMapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import reducers.FrequencyReducer;
+import reducers.TopKReducer;
 
-public class Task2 {
-    public class Task2 extends Configured implements Tool {
-        @Override
-        public int run(String[] strings) throws Exception {
-            Job job = Job.getInstance();
-            Configuration conf = HBaseConfiguration.create(job.getConfiguration());
-            conf.addResource("client-conf-ug.xml");
+public class Task2 extends Configured implements Tool {
+    @Override
+    public int run(String[] strings) throws Exception {
+        Job job = Job.getInstance();
+        Configuration conf = HBaseConfiguration.create(job.getConfiguration());
+        conf.addResource("client-conf-ug.xml");
+        conf.set("mapred.jar", "~/Desktop/bd4jar/AE2.jar");
+
+        Scan scan = new Scan();
+        scan.setBatch(100);
+        scan.setCaching(100);
+        scan.setCacheBlocks(false);
+        scan.addFamily(Bytes.toBytes("WD"));
+        //scan.setFilter(new FirstKeyOnlyFilter());
+        // TODO Add filter that checks dates.
+
+        job.setJobName("Task 2 - Stage 1");
+        job.setJarByClass(Task2.class);
+
+        TableMapReduceUtil.initTableMapperJob("BD4Project2", scan, FrequencyMapper.class, LongWritable.class, LongWritable.class, job);
+        job.setReducerClass(FrequencyReducer.class);
+
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(strings[0] + "-temp"));
+
+        job.submit();
+        if (job.waitForCompletion(true)) {
+            job = Job.getInstance();
+
+            job.setJobName("Task 2  - Stage 2");
+            job.getConfiguration().addResource("client-conf-ug.xml");
             conf.set("mapred.jar", "~/Desktop/bd4jar/AE2.jar");
 
-            Scan scan = new Scan();
-            scan.setBatch(100);
-            scan.setCaching(100);
-            scan.setCacheBlocks(false);
-            scan.addFamily(Bytes.toBytes("WD"));
-            //scan.setFilter(new FirstKeyOnlyFilter());
-            // TODO Add filter that checks dates.
+            job.getConfiguration().setInt("TopK", Integer.parseInt(strings[3]));
+            job.setMapperClass(TopKMapper.class);
+            job.setReducerClass(TopKReducer.class);
+            job.setNumReduceTasks(1);
 
-            job.setJobName("Task 2 - Stage 1");
-            job.setJarByClass(Task2.class);
+            job.setMapOutputKeyClass(ArticleRevCountWritable.class);
+            job.setMapOutputValueClass(NullWritable.class);
 
-            TableMapReduceUtil.initTableMapperJob("BD4Project2", scan, FrequencyMapper.class, LongWritable.class, LongWritable.class);
-            job.setReducerClass(FrequencyReducer.class);
+            job.setOutputKeyClass(ArticleRevCountWritable.class);
+            job.setOutputValueClass(NullWritable.class);
 
-            job.setOutputKeyClass(LongWritable.class);
-            job.setOutputValueClass(LongWritable.class);
-
-            FileOutputFormat.setOutputPath(job, new Path(strings[0]+"-temp"));
+            FileInputFormat.setInputPaths(job, new Path(strings[0] + "-temp"));
+            FileOutputFormat.setOutputPath(job, new Path(strings[0]));
 
             job.submit();
-            if (job.waitForCompletion(true)) {
-                job = Job.getInstance();
+            int retVal = job.waitForCompletion(true) ? 0 : 1;
 
-                job.setJobName("Task 2  - Stage 2");
-                job.getConfiguration().addResource("client-conf-ug.xml");
-                conf.set("mapred.jar", "~/Desktop/bd4jar/AE2.jar");
+            FilePrinter.printFile(strings[0]);
 
-                job.getConfiguration().setInt("TopK", Integer.parseInt(strings[3]));
-                job.setMapperClass(TopKMapper.class);
-                job.setReducerClass(TopKReducer.class);
-                job.setNumReduceTasks(1);
-
-                job.setMapOutputKeyClass(ArticleRevCountWritable.class);
-                job.setMapOutputValueClass(NullWritable.class);
-
-                job.setOutputKeyClass(ArticleRevCountWritable.class);
-                job.setOutputValueClass(NullWritable.class);
-
-                FileInputFormat.setInputPaths(job, new Path(strings[0]+"-temp"));
-                FileOutputFormat.setOutputPath(job, new Path(strings[0]));
-
-                job.submit();
-                int retVal = job.waitForCompletion(true) ? 0 : 1;
-
-                FilePrinter.printFile(strings[0]);
-
-                return retVal;
-            }
-
-            return 1;
+            return retVal;
         }
 
-        public static void main(String[] args) throws Exception {
-            System.exit(ToolRunner.run(new Configuration(), new Task2(), args));
-        }
+        return 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.exit(ToolRunner.run(new Configuration(), new Task2(), args));
     }
 }
--- AX2/src/tasks/Task3.java
+++ AX2/src/tasks/Task3.java
@@ -1,5 +1,64 @@
 package tasks;
 
-public class Task3 {
-    //TODO
+import helpers.FilePrinter;
+import helpers.TextArrayWritable;
+import mappers.BeforeTimeMapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import reducers.MostRecentReducer;
+
+public class Task3 extends Configured implements Tool {
+    @Override
+    public int run(String[] strings) throws Exception {
+        Configuration conf = HBaseConfiguration.create(getConf());
+
+        conf.addResource("client-conf-ug.xml");
+        conf.set("mapred.jar", "~/Desktop/bd4jar/AE2.jar");
+
+        Job job = Job.getInstance();
+
+        job.setJobName("Task 3");
+        job.setJarByClass(Task3.class);
+
+        Scan scan = new Scan();
+        scan.setBatch(100);
+        scan.setCaching(100);
+        scan.setCacheBlocks(false);
+        scan.addFamily(Bytes.toBytes("WD"));
+        //scan.setFilter(new FirstKeyOnlyFilter());
+        // TODO Add filter that checks dates.
+
+        TableMapReduceUtil.initTableMapperJob("BD4Project2Sample", scan, BeforeTimeMapper.class, LongWritable.class, TextArrayWritable.class, job);
+
+        job.setReducerClass(MostRecentReducer.class);
+
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        //job.setNumReduceTasks(1);
+
+        FileOutputFormat.setOutputPath(job, new Path(strings[0]));
+
+        job.submit();
+
+        int ret = job.waitForCompletion(true) ? 0 : 1;
+        FilePrinter.printFile(strings[0]);
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.exit(ToolRunner.run(new Configuration(), new Task1(), args));
+    }
 }
