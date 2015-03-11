828e87d17149ce61c01e50e93b074a53b3b2d3ce
Mircea Iordache <1106729i@student.gla.ac.uk>
2015-03-11 21:37:07 +0000

    In-Memory optimisation for Task2Mapper.

--- AX2/src/mappers/Task2Mapper.java
+++ AX2/src/mappers/Task2Mapper.java
@@ -17,30 +17,41 @@ import java.util.TreeMap;
 public class Task2Mapper extends TableMapper<LongWritable, LongWritable> {
     private TreeMap<Long, Long> map = new TreeMap<Long, Long>();
     private int topK;
-
+    private long lastArticleId;
+    private long revisionCount;
 
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         super.setup(context);
 
+        long lastArticleId = -1;
         topK = context.getConfiguration().getInt("TopK", 10);
     }
     
     public void map(ImmutableBytesWritable key, Result value, Context context) throws InterruptedException, IOException {
         byte[] row = value.getRow();
         long articleId = Bytes.toLong(Arrays.copyOfRange(row, 0, 8));
-        long revisionId = Bytes.toLong(Arrays.copyOfRange(row, 8, 16));
-        long revisionCount = 1;
 
-        if (map.containsKey(articleId)) {
-            revisionCount += map.get(articleId);
-            map.remove(articleId);
+        if (lastArticleId == -1) {
+            lastArticleId = articleId;
+            revisionCount = 0;
         }
 
-        map.put(articleId, revisionCount);
+        if (articleId != lastArticleId) {
+            map.put(lastArticleId, revisionCount);
+            lastArticleId = articleId;
+            revisionCount = 0;
+        }
+
+        revisionCount++;
+
+
     }
 
     protected void cleanup(Context context) throws IOException, InterruptedException {
+
+        if (revisionCount != 0)
+            map.put(lastArticleId, revisionCount);
 
         int i = 0;
 
